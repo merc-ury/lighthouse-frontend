03da9a0edbc9ad5dca840b832983e0e5f83c50af
Brandon Do <mbdx97@gmail.com>
2020-09-16 22:04:37 -0400

    removed eslint; added tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 const App: React.FC = () => {
+  const handleClick = (): void => {
+    alert('Hey!');
+  };
+
   return (
-    <div>Hello World!</div>
+    <Button variant="contained" color="primary" onClick={handleClick}>Test</Button>
   );
 };
 
