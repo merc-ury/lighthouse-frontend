091c9b4fa8f191faab9e42578dbbce3bb8ca1ad2
Brandon <mbdx97@gmail.com>
2020-09-29 20:18:15 -0400

    simple img

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, Button } from '@material-ui/core';
+import { Box, Container, Grid, Button, Typography } from '@material-ui/core';
 import mainLogo from '../assets/lighthouse.jpg';
 
 const useStyles = makeStyles({
@@ -8,7 +8,7 @@ const useStyles = makeStyles({
         maxWidth: 420
     },
     media: {
-        height: 500
+        height: 400
     }
 });
 
@@ -16,8 +16,16 @@ export const LandingPage: React.FC = () => {
     const classes = useStyles();
 
     return (
-        <Container>
-            <Button fullWidth>Test</Button>
-        </Container>
+        <Box>
+            <Grid container 
+                  direction="column"
+                  justify="center"
+                  alignItems="center">
+                <br />
+                <img className={classes.media} src={mainLogo} alt="lighthouse"/>
+                <Typography variant="h6">A light can be seen from the horizon...</Typography>
+                <Button variant="contained">Click here to sign up</Button>
+            </Grid>
+        </Box>
     );
 };
