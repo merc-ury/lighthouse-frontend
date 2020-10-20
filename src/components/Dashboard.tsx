50559beb3b054d39860d3286392b60884570a9cd
Brandon <mbdx97@gmail.com>
2020-10-20 18:54:20 -0400

    conditional rendering for dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { UserDataContext } from '../data/UserDataContext';
 
 export const Dashboard: React.FC = () => {
     const user = useContext(UserDataContext)[0];
+    const history = useHistory();
+    const [text, setText] = useState<string>('You need to log in first! Redirecting you in 3...');
 
     const renderPage = (): JSX.Element => {
         if (user.loggedIn)
             return (<Typography>Welcome {user.name}</Typography>);
-        else
-            return (<Typography>You need to log in!</Typography>);
+        else {
+            redirectToHome();
+            return (<Typography>{text}</Typography>);
+        }
+    };
+
+    const redirectToHome = (): void => {
+        
     };
 
     return renderPage();
