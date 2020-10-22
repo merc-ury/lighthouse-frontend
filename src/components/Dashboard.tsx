d87914f62e0a4c2255d739b1e412b5d165bc30c5
Brandon Do <mbdx97@gmail.com>
2020-10-21 20:21:45 -0400

    redirect back to homepage

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,24 +1,29 @@
-import React, { useContext, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useContext, useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { UserDataContext } from '../data/UserDataContext';
 
 export const Dashboard: React.FC = () => {
     const user = useContext(UserDataContext)[0];
-    const history = useHistory();
-    const [text, setText] = useState<string>('You need to log in first! Redirecting you in 3...');
+    const [count, setCount] = useState<number>(3);
+    const [text, setText] = useState<string>(`You need to log in first! Redirecting you in ${count}...`);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            console.log('TIMER IS ACTIVATED...?');
+            setText(`You need to log in first! Redirecting you in ${count}...`);
+            setCount(count - 1);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
 
     const renderPage = (): JSX.Element => {
         if (user.loggedIn)
             return (<Typography>Welcome {user.name}</Typography>);
         else {
-            redirectToHome();
-            return (<Typography>{text}</Typography>);
+            return count === 0 ? (<Redirect to="/" />) : (<Typography>{text}</Typography>);
         }
-    };
-
-    const redirectToHome = (): void => {
-        
     };
 
     return renderPage();
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,19 @@ import React, { useContext } from 'react';
 import { UserDataContext } from '../data/UserDataContext';
 import { MenuBar } from './MenuBar';
 import { Link } from 'react-router-dom';
-import { Dashboard } from './Dashboard';
-import { AboutPage } from './AboutPage';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Box, Toolbar, Typography } from '@material-ui/core';
+import theme from '../theme';
 
 const useStyles = makeStyles({
     root: {
         flexGrow: 1
     },
     title: {
+        display: 'inline',
+        margin: theme.spacing(2)
+    },
+    menuBar: {
         flexGrow: 1
     }
 });
@@ -25,12 +28,14 @@ export const Navbar: React.FC = () => {
             <AppBar position="static">
                 <Toolbar>
                     <Typography className={classes.title} variant="h6">Lighthouse Notes</Typography>
-                    <Button component={Link} to={"/dashboard"} color="inherit">
-                        Dashboard
-                    </Button>
-                    <Button component={Link} to={"/about"} color="inherit">
-                        About
-                    </Button>
+                    <Box className={classes.menuBar}>
+                        <Button component={Link} to={"/dashboard"} color="inherit">
+                            Dashboard
+                        </Button>
+                        <Button component={Link} to={"/about"} color="inherit">
+                            About
+                        </Button>
+                    </Box>
                     <MenuBar user={user} />
                 </Toolbar>
             </AppBar>
